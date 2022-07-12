09a5c4162b0b84f0bb6d6dc881ce18f2f9f20c6f
Roman Surkov <s.rb@list.ru>
2022-07-12 20:25:05 +0300

    Использование refs вместо стейт для получение значений

--- src/components/Users/CreateUser.js
+++ src/components/Users/CreateUser.js
@@ -1,39 +1,36 @@
 import Card from "../UI/Card";
 import styles from "./CreateUser.module.css";
 import Button from "../UI/Button";
-import React, {useState, Fragment} from "react";
+import React, {Fragment, useRef, useState} from "react";
 import ErrorModal from "../UI/ErrorModal";
 
 const CreateUser = (props) => {
 
-    const [inputName, setInputName] = useState('');
-    const [inputAge, setInputAge] = useState('');
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+
     const [error, setError] = useState();
 
     const createUserHandler = (event) => {
         event.preventDefault();
-        if (inputName.trim().length === 0 || inputAge.trim().length === 0) {
+        const inputUserName = nameInputRef.current.value;
+        const inputUserAge = ageInputRef.current.value;
+        if (inputUserName.trim().length === 0 || inputUserAge.trim().length === 0) {
             setError({title: 'Некорректный ввод', message: 'Эти поля не могут быть пустыми'});
             return;
         }
 
         // + - преобразует строку в число
-        if (+inputAge < 1) {
+        if (+inputUserAge < 1) {
             setError({title: 'Некорректный возраст', message: 'Возраст должен быть больше 0'});
             return;
         }
 
-        props.onCreateUser(inputName, inputAge);
-        setInputName("");
-        setInputAge("");
+        props.onCreateUser(inputUserName, inputUserAge);
+        // Не рекомендуется делать так, лучше State. Для считывания - refs норм
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';
     };
-
-    const nameChangeHandler = (event) => {
-        setInputName(event.target.value);
-    }
-    const ageChangeHandler = (event) => {
-        setInputAge(event.target.value);
-    }
 
     const errorHandler = () => {
         setError(false);
@@ -45,9 +42,9 @@ const CreateUser = (props) => {
             <Card className={styles.input}>
                 <form onSubmit={createUserHandler}>
                     <label htmlFor='name'>Имя</label>
-                    <input type='text' id='name' onChange={nameChangeHandler} value={inputName}/>
+                    <input type='text' id='name' ref={nameInputRef}/>
                     <label htmlFor='age'>Возраст</label>
-                    <input type='number' id='age' onChange={ageChangeHandler} value={inputAge}/>
+                    <input type='number' id='age' ref={ageInputRef}/>
                     <Button type='submit'>Добавить пользователя</Button>
                 </form>
             </Card>
