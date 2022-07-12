36b61af18d6b0f3c119af47f818896540125dcb3
Roman Surkov <s.rb@list.ru>
2022-07-12 14:52:12 +0300

    Переделка с нуля добавления пользователей. Форма добавления

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,10 +1,9 @@
-import "./Card.css";
+ import styles from "./Card.module.css";
 
 const Card = (props) => {
-    const classes = 'card ' + props.className;
-    return (
-        <div className={classes}>{props.children}</div>
-    )
-}
+    return <div className={`${styles.card} ${props.className}`}>
+        {props.children}
+    </div>
+ }
 
-export default Card;
+ export default Card;
