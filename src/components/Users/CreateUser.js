edd5b4ebb96dffca2613afdd656b3a6222cc791b
Roman Surkov <s.rb@list.ru>
2022-07-12 15:32:50 +0300

    Получение данных ввода, валидация их и очистка формы ввода

--- src/components/Users/CreateUser.js
+++ src/components/Users/CreateUser.js
@@ -1,20 +1,43 @@
 import Card from "../UI/Card";
 import styles from "./CreateUser.module.css";
 import Button from "../UI/Button";
+import {useState} from "react";
 
 const CreateUser = (props) => {
 
+    const [inputName, setInputName] = useState('')
+    const [inputAge, setInputAge] = useState('')
+
     const createUserHandler = (event) => {
         event.preventDefault();
+        if (inputName.trim().length === 0 || inputAge.trim().length === 0) {
+            return;
+        }
+
+        // + - преобразует строку в число
+        if (+inputAge < 1) {
+            return;
+        }
+
+        console.log(inputName, inputAge)
+        setInputName("");
+        setInputAge("");
     };
+
+    const nameChangeHandler = (event) => {
+        setInputName(event.target.value);
+    }
+    const ageChangeHandler = (event) => {
+        setInputAge(event.target.value);
+    }
 
     return (
         <Card className={styles.input}>
             <form onSubmit={createUserHandler}>
                 <label htmlFor='name'>Имя</label>
-                <input type='text' id='name'/>
-                <label htmlFor='age'>Имя</label>
-                <input type='number' id='age'/>
+                <input type='text' id='name' onChange={nameChangeHandler} value={inputName}/>
+                <label htmlFor='age'>Возраст</label>
+                <input type='number' id='age' onChange={ageChangeHandler} value={inputAge}/>
                 <Button type='submit'>Добавить пользователя</Button>
             </form>
         </Card>
